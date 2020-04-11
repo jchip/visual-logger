243303e8d1150cf305d20a8e40245466a2ad0384
Joel Chen <xchen@walmartlabs.com>
2020-04-11 16:30:46 -0700

    use a single interval timer for spinners

--- test/spec/visual-logger.spec.js
+++ test/spec/visual-logger.spec.js
@@ -2,6 +2,8 @@
 
 const chalk = require("chalk");
 const VisualLogger = require("../..");
+const { asyncVerify } = require("run-verify");
+const { delay } = require("xaa");
 
 describe("visual-logger", function() {
   it("should init with default color setting to true", () => {
@@ -81,6 +83,7 @@ describe("visual-logger", function() {
     let vis;
     let visList;
     let visLog;
+
     const make = opts => {
       out = [];
       clearCount = 0;
@@ -111,7 +114,8 @@ describe("visual-logger", function() {
                   clearCount++;
                 }
               }
-            }
+            },
+            renderFps: 30
           },
           opts
         )
@@ -124,10 +128,21 @@ describe("visual-logger", function() {
       visLog.addItem(itemOpt);
     });
 
+    afterEach(() => {
+      visLog.shutdown();
+    });
+
     const update1 = () => {
-      visLog.updateItem("TEST_1", "hello");
-      expect(visLog.hasItem("TEST_1")).to.equal(true);
-      expect(vis).to.equal("TEST_1: hello");
+      return asyncVerify(
+        () => {
+          visLog.updateItem("TEST_1", "hello");
+        },
+        () => delay(40),
+        () => {
+          expect(visLog.hasItem("TEST_1")).to.equal(true);
+          expect(vis).to.equal("TEST_1: hello");
+        }
+      );
     };
 
     const add2 = display => {
@@ -157,78 +172,105 @@ describe("visual-logger", function() {
         .addItem(itemOpt)
         .updateItem("foo")
         .updateItem("TEST_1", "hello");
-      expect(vis).to.equal("TEST_1: hello");
-      visLog.error("error message");
-      expect(clearCount).to.equal(1);
-      expect(out).to.deep.equal(["> error message\n"]);
-      add2();
-      visLog.updateItem("TEST_1"); // should do nothing w/o data
-      expect(vis).to.equal("TEST_1: hello\nTEST_2: world");
+      return asyncVerify(
+        () => delay(120),
+        () => {
+          expect(vis).to.equal("TEST_1: hello");
+          visLog.error("error message");
+          expect(clearCount).to.equal(1);
+          expect(out).to.deep.equal(["> error message\n"]);
+          add2();
+          visLog.updateItem("TEST_1"); // should do nothing w/o data
+        },
+        () => delay(50),
+        () => {
+          expect(vis).to.equal("TEST_1: hello\nTEST_2: world");
+        }
+      );
     });
 
     it("should removeItem", () => {
-      update1();
-      add2();
-      visLog.removeItem("bad").removeItem("TEST_2");
-      expect(visLog.hasItem("TEST_1"), "should have TEST_1 item").to.equal(true);
-      expect(visLog.hasItem("TEST_2"), "should have TEST_2 item").to.equal(false);
+      return asyncVerify(
+        () => update1(),
+        () => {
+          add2();
+          visLog.removeItem("bad").removeItem("TEST_2");
+        },
+        () => {
+          expect(visLog.hasItem("TEST_1"), "should have TEST_1 item").to.equal(true);
+          expect(visLog.hasItem("TEST_2"), "should have TEST_2 item").to.equal(false);
+        }
+      );
     });
 
     it("should freeItem and stop updating them", () => {
-      update1();
-      visLog.freezeItems(false);
-      expect(vis).to.equal(undefined);
-      out = [];
-      visLog.freezeItems(true);
-      expect(out[0]).to.equal("TEST_1: hello\n");
+      return asyncVerify(
+        () => update1(),
+        () => visLog.freezeItems(false),
+        () => expect(vis).to.equal(undefined),
+        () => (out = []),
+        () => visLog.freezeItems(true),
+        () => expect(out[0]).to.equal("TEST_1: hello\n")
+      );
     });
 
-    it("should handle auto spinners", done => {
+    it("should handle auto spinners", () => {
       visLog.addItem({ name: "T", spinner: VisualLogger.spinners[0], color: "blue" });
-      setTimeout(() => {
-        expect(visList.slice(0, 4)).to.deep.equal([
-          "TEST_1: \n/ T: ",
-          "TEST_1: \n- T: ",
-          "TEST_1: \n\\ T: ",
-          "TEST_1: \n| T: "
-        ]);
-        visLog.removeItem("T");
-        visList = [];
-        visLog.updateItem("TEST_1", { msg: "1" });
-        expect(visList).to.deep.equal(["TEST_1: 1"]);
-        done();
-      }, 500);
+      return asyncVerify(
+        () => delay(500),
+        () => {
+          expect(visList.slice(0, 4)).to.deep.equal([
+            "TEST_1: \n| T: ",
+            "TEST_1: \n/ T: ",
+            "TEST_1: \n- T: ",
+            "TEST_1: \n\\ T: "
+          ]);
+          visLog.removeItem("T");
+          visList = [];
+          visLog.updateItem("TEST_1", { msg: "1" });
+        },
+        () => delay(50),
+        () => {
+          expect(visList).to.deep.equal(["TEST_1: 1"]);
+        }
+      );
     });
 
-    it("should handle spinner being true", done => {
+    it("should handle spinner being true", () => {
       const spinner = VisualLogger.spinners[1];
       visLog.addItem({ name: "T", spinner: true, color: "blue" });
-      setTimeout(() => {
-        const expected = [`TEST_1: \n${spinner[0]} T: `];
-        expect(visList.slice(0, 1)).to.deep.equal(expected);
-        visLog.removeItem("T");
-        visList = [];
-        visLog.updateItem("TEST_1", { msg: "1" });
-        expect(visList).to.deep.equal(["TEST_1: 1"]);
-        done();
-      }, 150);
+      return asyncVerify(
+        () => delay(150),
+        () => {
+          const expected = [`TEST_1: \n${spinner[0]} T: `];
+          expect(visList.slice(0, 1)).to.deep.equal(expected);
+          visLog.removeItem("T");
+          visList = [];
+          visLog.updateItem("TEST_1", { msg: "1" });
+        },
+        () => delay(50),
+        () => {
+          expect(visList).to.deep.equal(["TEST_1: 1"]);
+        }
+      );
     });
 
-    it("should handle spinner being a valid index", done => {
+    it("should handle spinner being a valid index", () => {
       const spinner = VisualLogger.spinners[2];
       const opts = { name: "T", spinner: 2, color: "blue" };
       visLog.addItem(opts);
-      setTimeout(() => {
-        // well, we have a bit of a start off-by-1 issue, but whatever, it's just
-        // circular spinning
-        const expected = [`TEST_1: \n${spinner[1]} T: `];
-        expect(visList.slice(0, 1)).to.deep.equal(expected);
-        visLog.removeItem("T");
-        visList = [];
-        visLog.updateItem("TEST_1", { msg: "1" });
-        expect(visList).to.deep.equal(["TEST_1: 1"]);
-        done();
-      }, 150);
+      return asyncVerify(
+        () => delay(150),
+        () => {
+          const expected = [`TEST_1: \n${spinner[0]} T: `];
+          expect(visList.slice(0, 1)).to.deep.equal(expected);
+          visLog.removeItem("T");
+          visList = [];
+          visLog.updateItem("TEST_1", { msg: "1" });
+        },
+        () => delay(50),
+        () => expect(visList).to.deep.equal(["TEST_1: 1"])
+      );
     });
 
     it("should not start spinner if item type is not normal", () => {
@@ -237,106 +279,229 @@ describe("visual-logger", function() {
       expect(visLog._itemOptions.S.spinTimer).to.equal(undefined);
     });
 
-    it("should freeze/unfreeze items", done => {
+    it("should freeze/unfreeze items and circular through them", () => {
       visList = [];
-      visLog.addItem({ name: "T", spinner: VisualLogger.spinners[0], color: "blue" });
+      visLog.addItem({ name: "T", spinner: "abc", color: "blue" });
+      visLog.updateItem("T", "foo"); // this will trigger a render to output
+      return asyncVerify(
+        () => delay(350),
+        () => {
+          expect(visList.slice(0, 3)).to.deep.equal([
+            "TEST_1: \na T: foo", // trigger by updateItem above
+            "TEST_1: \na T: foo", // trigger by first spin
+            "TEST_1: \nb T: foo"
+          ]);
+          visList = [];
+          visLog.freezeItems();
+        },
+        () => delay(150),
+        () => {
+          expect(visList).to.deep.equal([]);
+          visLog.unfreezeItems();
+          visLog.unfreezeItems();
+        },
+        () => delay(500),
+        () => {
+          expect(visList.slice(0, 5)).to.deep.equal([
+            "TEST_1: \na T: foo",
+            "TEST_1: \nb T: foo",
+            "TEST_1: \nc T: foo",
+            "TEST_1: \na T: foo"
+          ]);
+        }
+      );
+    });
+
+    it("should reset item spin index to 0 in addItem", () => {
+      visList = [];
+      visLog.addItem({ name: "T", spinner: "abc", color: "blue" });
       visLog.updateItem("T", "foo");
-      setTimeout(() => {
-        expect(visList.slice(0, 2)).to.deep.equal(["TEST_1: \n| T: foo", "TEST_1: \n/ T: foo"]);
-        visLog.freezeItems();
-        setTimeout(() => {
-          expect(visList.slice(0, 2)).to.deep.equal(["TEST_1: \n| T: foo", "TEST_1: \n/ T: foo"]);
-          visLog.unfreezeItems();
-          visLog.unfreezeItems();
-          setTimeout(() => {
-            expect(visList.slice(0, 3)).to.deep.equal([
-              "TEST_1: \n| T: foo",
-              "TEST_1: \n/ T: foo",
-              "TEST_1: \n- T: foo"
-            ]);
-            done();
-          }, 115);
-        }, 100);
-      }, 150);
+      return asyncVerify(
+        () => delay(300),
+        () => {
+          expect(visList.slice(0, 3)).to.deep.equal([
+            "TEST_1: \na T: foo",
+            "TEST_1: \na T: foo",
+            "TEST_1: \nb T: foo"
+          ]);
+          visLog.addItem({ name: "R", spinner: "xyz" });
+        },
+        () => delay(100),
+        () => {
+          expect(visList.slice(0, 4)).to.deep.equal([
+            "TEST_1: \na T: foo",
+            "TEST_1: \na T: foo",
+            "TEST_1: \nb T: foo",
+            "TEST_1: \na T: foo\nx R: "
+          ]);
+        }
+      );
+    });
+
+    it("should support independent spin timer for different interval", () => {
+      visList = [];
+      visLog.addItem({ name: "R", spinner: "xyz", color: "blue" });
+      visLog.addItem({ name: "T", spinner: "abc", color: "blue", spinInterval: 50 });
+      visLog.updateItem("T", "foo");
+      return asyncVerify(
+        () => delay(200),
+        () => {
+          expect(visList.slice(0, 4)).to.deep.equal([
+            "TEST_1: \nx R: \na T: foo",
+            "TEST_1: \nx R: \na T: foo",
+            "TEST_1: \nx R: \nb T: foo",
+            "TEST_1: \nx R: \nc T: foo"
+          ]);
+          visLog.removeItem("T");
+        },
+        () => delay(150),
+        () => {
+          expect(visList.slice(0, 6)).to.deep.equal([
+            "TEST_1: \nx R: \na T: foo",
+            "TEST_1: \nx R: \na T: foo",
+            "TEST_1: \nx R: \nb T: foo",
+            "TEST_1: \nx R: \nc T: foo",
+            "TEST_1: \ny R: ",
+            "TEST_1: \nz R: "
+          ]);
+        }
+      );
     });
 
     it("should freeze items and show correct current render when item type is simple", () => {
-      visLog.addItem({ name: "T", spinner: VisualLogger.spinners[0], color: "blue" });
-      visLog.setItemType("simple");
-      visLog.updateItem("T", "hello world");
-      visLog.freezeItems(true);
-      expect(out).to.deep.equal([".\n", "TEST_1: \nT: hello world\n"]);
+      return asyncVerify(
+        () => {
+          visLog.addItem({ name: "T", spinner: VisualLogger.spinners[0], color: "blue" });
+          visLog.setItemType("simple");
+          visLog.updateItem("T", "hello world");
+          visLog.freezeItems(true);
+        },
+        () => delay(50),
+        () => {
+          expect(out).to.deep.equal([".\n", "TEST_1: \nT: hello world\n"]);
+        }
+      );
     });
 
     it("should use display from data when updateItem", () => {
-      update1();
-      visLog.updateItem("TEST_1", { msg: "foo", display: "bar" });
-      expect(vis).to.equal("bar: foo");
+      return asyncVerify(
+        () => update1(),
+        () => visLog.updateItem("TEST_1", { msg: "foo", display: "bar" }),
+        () => delay(50),
+        () => expect(vis).to.equal("bar: foo")
+      );
     });
 
     it("should not save to log data if it's off", () => {
       visLog._options.saveLogs = false;
-      update1();
-      expect(visLog.logData).to.deep.equal([]);
+      return asyncVerify(
+        () => update1(),
+        () => {
+          expect(visLog.logData).to.deep.equal([]);
+        }
+      );
     });
 
     it("should not log item if type is none", () => {
-      visLog.setItemType("none");
-      visLog.updateItem("TEST_1", "hello");
-      expect(out).to.deep.equal([]);
-      expect(visList).to.deep.equal([]);
+      return asyncVerify(
+        () => {
+          visLog.setItemType("none");
+          visLog.updateItem("TEST_1", "hello");
+        },
+        () => delay(50),
+        () => {
+          expect(out).to.deep.equal([]);
+          expect(visList).to.deep.equal([]);
+        }
+      );
     });
 
     it("should log dots if item type is simple", () => {
-      visLog = make({ maxDots: 10 });
-      visLog.addItem(itemOpt);
-      visLog.setItemType("simple");
-      visLog.updateItem("TEST_1", "hello");
-      expect(out).to.deep.equal(["."]);
-      expect(visList).to.deep.equal([]);
-      out = [];
-      visLog.clearItems();
-      expect(out).to.deep.equal([]);
-      expect(visList).to.deep.equal([]);
-      for (let i = 0; i < 15; i++) {
-        visLog.updateItem("TEST_1", `${i}`);
-      }
-      expect(out).to.deep.equal([".........\n", "......"]);
+      return asyncVerify(
+        () => {
+          visLog = make({ maxDots: 10 });
+          visLog.addItem(itemOpt);
+          visLog.setItemType("simple");
+          visLog.updateItem("TEST_1", "hello");
+        },
+        () => delay(50),
+        () => {
+          expect(out).to.deep.equal(["."]);
+          expect(visList).to.deep.equal([]);
+          out = [];
+          visLog.clearItems();
+        },
+        () => delay(50),
+        () => {
+          expect(out).to.deep.equal([]);
+          expect(visList).to.deep.equal([]);
+          for (let i = 0; i < 15; i++) {
+            visLog.updateItem("TEST_1", `${i}`);
+          }
+        },
+        () => delay(50),
+        () => {
+          expect(out).to.deep.equal([".........\n", "......"]);
+        }
+      );
     });
 
     it("should log less dots if updatesPerDot is > 1", () => {
-      visLog = make({ maxDots: 10, updatesPerDot: undefined });
-      visLog.addItem(itemOpt);
-      visLog.setItemType("simple");
-      visLog.updateItem("TEST_1", "hello");
-      expect(out).to.deep.equal([]);
-      expect(visList).to.deep.equal([]);
-      out = [];
-      visLog.clearItems();
-      expect(out).to.deep.equal([]);
-      expect(visList).to.deep.equal([]);
-      for (let i = 0; i < 15; i++) {
-        visLog.updateItem("TEST_1", `${i}`);
-      }
-      expect(out).to.deep.equal(["..."]);
+      return asyncVerify(
+        () => {
+          visLog = make({ maxDots: 10, updatesPerDot: undefined });
+          visLog.addItem(itemOpt);
+          visLog.setItemType("simple");
+          visLog.updateItem("TEST_1", "hello");
+        },
+        () => delay(50),
+        () => {
+          expect(out).to.deep.equal([]);
+          expect(visList).to.deep.equal([]);
+          out = [];
+          visLog.clearItems();
+        },
+        () => delay(150),
+        () => {
+          expect(out).to.deep.equal([]);
+          expect(visList).to.deep.equal([]);
+        },
+        () => {
+          for (let i = 0; i < 15; i++) {
+            visLog.updateItem("TEST_1", `${i}`);
+          }
+        },
+        () => delay(150),
+        () => expect(out).to.deep.equal(["..."])
+      );
     });
 
     it("should not save to log data if flag is false", () => {
-      visLog.updateItem("TEST_1", { msg: "blah", _save: false });
-      expect(visLog.logData).to.deep.equal([]);
-      visLog.addItem({ name: "B", save: false });
-      visLog.updateItem("B", "hello");
-      expect(visList).to.deep.equal(["TEST_1: blah", "TEST_1: blah\nB: hello"]);
-      expect(visLog.logData).to.deep.equal([]);
+      return asyncVerify(
+        () => visLog.updateItem("TEST_1", { msg: "blah", _save: false }),
+        () => expect(visLog.logData).to.deep.equal([]),
+        () => visLog.addItem({ name: "B", save: false }),
+        () => visLog.updateItem("B", "hello"),
+        () => delay(150),
+        () => {
+          expect(visList).to.deep.equal(["TEST_1: blah\nB: hello"]);
+          expect(visLog.logData).to.deep.equal([]);
+        }
+      );
     });
 
     it("should render if _render flag is false", () => {
-      visLog.updateItem("TEST_1", { msg: "blah", _save: false, _render: false });
-      expect(visLog.logData).to.deep.equal([]);
-      visLog.addItem({ name: "B", save: false });
-      visLog.updateItem("B", "hello");
-      expect(visList).to.deep.equal(["TEST_1: \nB: hello"]);
-      expect(visLog.logData).to.deep.equal([]);
+      return asyncVerify(
+        () => visLog.updateItem("TEST_1", { msg: "blah", _save: false, _render: false }),
+        () => expect(visLog.logData).to.deep.equal([]),
+        () => visLog.addItem({ name: "B", save: false }),
+        () => visLog.updateItem("B", "hello"),
+        () => delay(50),
+        () => {
+          expect(visList).to.deep.equal(["TEST_1: \nB: hello"]);
+          expect(visLog.logData).to.deep.equal([]);
+        }
+      );
     });
   });
 });
